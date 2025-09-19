c2b55933f1f2db6baa957a1d21246c17842258da
alduin0 <alduin@posteo.de>
2025-09-19 16:07:16 +0200

    allocation memory

--- src/func_create_dataset.py
+++ src/func_create_dataset.py
@@ -1,3 +1,5 @@
+from encodings.punycode import selective_len
+
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import yfinance as yf
@@ -22,35 +24,44 @@ def create_dataset(ticker:tuple[str]|list[str]=('DB1.DE', '^STOXX50E'),
     mylog.info(f"Called function: {inspect.stack()[0].function}")
 
     # ================================ GETTING MULTITICKER DATA AT ONCE ================================================
-    mytickers = yf.Tickers(ticker)
-    histories = mytickers.history(period=f"{days}d")
+    mytickers = yf.Tickers(ticker)                                             #
+    histories = mytickers.history(period=f"{days}d")                           #
     # instead of dropping nan rows, implement data filling later
-    histories = histories.drop(columns=['High', 'Low', 'Close', 'Volume', 'Dividends', 'Stock Splits'])
-    rows_before = len(histories)
-    histories = histories.dropna()
-    rows_after = len(histories)
+    histories = histories.drop(columns=['High', 'Low', 'Close',                #
+                                        'Volume', 'Dividends',                 #
+                                        'Stock Splits'])                       #
+    rows_before = len(histories)                                               #
+    histories = histories.dropna()                                             #
+    rows_after = len(histories)                                                #
     mylog.info(f" {(rows_after - rows_before) / rows_before * 100:.2f} [%] were removed by dropna().")
     mylog.info(f"Using {rows_after} rows for upcoming data preparation.")
     # logging the timespan covered in terms of dates in real world
-    dates = histories.index
-    mylog.info(f"Used data covers dates from {dates.min()} to {dates.max()}")
-    histories = histories['Open']
+    dates = histories.index                                                    #
+    mylog.info(f"Used data covers dates from {dates.min()} to {dates.max()}")  #
+    histories = histories['Open']                                              #
     # ============================= SCALING DATA TO [0,1] ==============================================================
-    for key in histories.keys():
-        histories[key] = MinMaxScaler(feature_range=(0,1)).fit_transform(histories[key].values.reshape(-1,1))
-    hist_val = histories.values
-    print(hist_val.shape)
+    for key in histories.keys():                                               #
+        histories[key] = MinMaxScaler(feature_range=(0,1)).fit_transform(      #
+                                      histories[key].values.reshape(-1,1))     #
+    hist_val = histories.values                                                #
     # ============================== SLICING FRAME BLOCKS INTO HISTORY SEQUENCES =======================================
-    index_splits = np.array((split[0], split[0]+split[1]))/100 * rows_after
-    index_splits = [int(val) for val in index_splits]
+    index_splits = np.array((split[0], split[0]+split[1]))/100 * rows_after    #
+    index_splits = [int(val) for val in index_splits]                          #
 
-    data_train = hist_val[:index_splits[0],:]
-    data_val   = hist_val[index_splits[0]:index_splits[1], :]
-    data_test  = hist_val[index_splits[1]::, :]
+    data_train = hist_val[:index_splits[0],:]                                  #
+    data_val   = hist_val[index_splits[0]:index_splits[1], :]                  #
+    data_test  = hist_val[index_splits[1]::, :]                                #
 
-    size_train = index_splits[0]
-    size_val = index_splits[1]
-    size_test = rows_after - size_val - size_train
+    size_train = index_splits[0] - seq_len                                     #
+    size_val = index_splits[1] - seq_len                                       #
+    size_test = rows_after - size_val - size_train - seq_len                   #
+    size_cols = len(ticker) * seq_len                                          #
 
+    x_train, y_train = np.zeros((size_train, size_cols),
+                                dtype=np.float64)
+    x_val, y_val = np.zeros((size_val, size_cols),
+                            dtype=np.float64)
+    x_test, y_test = np.zeros((size_test, size_cols),
+                              dtype=np.float64)
 
     return {}
